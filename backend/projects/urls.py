116cf7fa53330204fd07d0609ef4abca4e860fad
tosta <tobias.stach@gmail.com>
2023-08-14 15:23:57 +0200

    formatted

--- backend/projects/urls.py
+++ backend/projects/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from .views import ProjectList, ProjectDetail
 
-app_name = 'projects'
+app_name = "projects"
 
 urlpatterns = [
-    path('<int:pk>/', ProjectDetail.as_view(), name="project-detail"),
-    path('', ProjectList.as_view(), name="project-list"),
+    path("<int:pk>/", ProjectDetail.as_view(), name="project-detail"),
+    path("", ProjectList.as_view(), name="project-list"),
 ]
--- backend/projects/views.py
+++ backend/projects/views.py
@@ -2,12 +2,12 @@ from rest_framework import generics
 from .models import Project
 from .serializers import ProjectSerializer
 
-# Create your views here.
+
 class ProjectList(generics.ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
+
 class ProjectDetail(generics.RetrieveDestroyAPIView):
     queryset = Project.objects.filter()
     serializer_class = ProjectSerializer
-    

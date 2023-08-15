72915be59722599b256cf8de993780f55f91a49e
tosta <tobias.stach@gmail.com>
2023-08-15 15:03:51 +0200

    custom user model base setup

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    list_display = ["email", "is_staff","is_admin"]
+    list_filter = ["is_admin"]
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Permissions", {"fields": ["is_admin", "is_staff"]}),
+    ]
+
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)

2970907d2caafb46cb456f266fe2c7f34851b770
tosta <tobias.stach@gmail.com>
2023-08-15 18:15:03 +0200

    frontend setup and header/footer components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header/header.vue'
+import Footer from './components/Footer/footer.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="DRF + Vite + Vue" />
+  <body class="min-h-screen pt-20 bg-black">
+    <Header />
+    <main class="h-screen mb-auto"></main>
+    <Footer />
+  </body>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>

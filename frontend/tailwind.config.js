adf1e7f785954e469233909990c2a7ce9a4f1936
tosta <tobias.stach@gmail.com>
2023-08-23 13:28:05 +0200

    primary color light/dark added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      light: {
+        main: '#FFFFF',
+        primary: {
+          DEFAULT: '#2C00CC',
+          100: '#463EC5',
+          200: '#5F7BBD',
+        },
+
+      },
+      dark: {
+        DEFAULT: '#55B3FF',
+          100: '#0A91FF',
+          200: '#0065B8',
+      },
+    }
   },
   plugins: [],
 };

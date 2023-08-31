0dd426629d0ed38ef616425ef8a7318a7fa330d2
Joel Ducram <J.ducram@gmail.com>
2023-08-31 21:41:06 +0100

    changes for views CBV to FBV

--- backend/apps/projects/urls.py
+++ backend/apps/projects/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import ProjectList, ProjectDetail
+from .views import project_detail, project_list
 
 app_name = "projects"
 
 urlpatterns = [
-    path("<int:pk>/", ProjectDetail.as_view(), name="project-detail"),
-    path("", ProjectList.as_view(), name="project-list"),
+    path("<int:pk>/", project_detail, name="project-detail"),
+    path("", project_list, name="project-list"),
 ]

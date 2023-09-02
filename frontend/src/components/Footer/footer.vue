95851efed56ee10243200f707484b77ae23bd680
tosta <tobias.stach@gmail.com>
2023-09-02 14:26:51 +0200

    frontend formatted with prettier

--- frontend/src/components/Footer/footer.vue
+++ frontend/src/components/Footer/footer.vue
@@ -1,13 +1,13 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <footer class="bottom-0 left-0 w-full bg-white dark:bg-gray-800">
-        <div class="w-full max-w-screen-xl p-4 mx-auto md:flex md:items-center md:justify-between">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023<a href="#"
-                    class="hover:underline">Code Project</a>
-            </span>
-        </div>
-    </footer>
+  <footer class="bottom-0 left-0 w-full bg-white dark:bg-gray-800">
+    <div
+      class="w-full max-w-screen-xl p-4 mx-auto md:flex md:items-center md:justify-between"
+    >
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+        >© 2023<a href="#" class="hover:underline">Code Project</a>
+      </span>
+    </div>
+  </footer>
 </template>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,10 +1,7 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div id="home">
-        <h1 class="text-2xl text-white">Home</h1>
-    </div>
+  <div id="home">
+    <h1 class="text-2xl text-white">Home</h1>
+  </div>
 </template>
-
